bef4ac81dcc735f884f46847638f30ecdfd6d28c
sesmaniraj <sesmaniraj7@gmail.com>
2024-04-14 10:57:46 +0545

    feat(login) : user login successfully and secure the login with the help of json web token and store in cookie with specific expire time

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.models.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -10,5 +11,30 @@ export const registerUser = async (req, res) => {
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Invalid password");
+    }
+    const token = jwt.sign({ userId: user._id }, "secret_key", {
+      expiresIn: "1h",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 3600000,
+      //   secure: process.env.NODE_ENV === "production",
+    });
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
   }
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
