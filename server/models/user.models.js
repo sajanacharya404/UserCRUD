f2fce08651dfc1ac95d18ded336192a62af3deca
sesmaniraj <sesmaniraj7@gmail.com>
2024-04-14 10:43:20 +0545

    feat:user model created for registeration and login

---
+++ server/models/user.models.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+const User = mongoose.model("User", userSchema);
+
+export default User;
